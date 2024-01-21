e1f5062f5073e8b7162a3010efc3dffdc48a698d
Berk <139360217+Beko2023@users.noreply.github.com>
2024-01-21 10:42:48 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+document.getElementById("view-more").addEventListener("click", function () {
+  document.getElementById("extra-articles").style.display = "flex";
+  document.getElementById("view-more").style.display = "none";
+});
